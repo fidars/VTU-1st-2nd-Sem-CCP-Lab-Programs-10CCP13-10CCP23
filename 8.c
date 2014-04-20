2045dfc5f4d7b96fcfd3810494b0e955ec4c4cae
Kolli Shivagangadhar <shivagangadhar6@gmail.com>
2014-04-20 11:26:02 +0530

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,15 @@
+/*8. Design, develop and execute a program in C to compute and print the word length on the
+host machine. */
+
+
+#include<stdio.h>
+void main()
+{
+	int var = -1,wordlen=0;
+	while (var)
+	{
+		wordlen++;
+		var <<= 1;
+	}
+	printf(" The Word length of this Host Machine is %d Bits", wordlen);
+}
