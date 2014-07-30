5bb794c76afaf9ae2bbd09ac629b224531ef357c
Shivagangadhar Kolli <shivagangadhar6@gmail.com>
2014-07-30 20:34:31 +0530

    15th program

---
+++ P015.c
@@ -0,0 +1,28 @@
+/*15. Design and develop a function reverses(s) in C to reverse the string s in place. Invoke this
+function from the main for different strings and print the original and reversed strings. */
+
+#include <stdio.h>
+#include <string.h>
+void reverse(char s[])
+{
+	char temp;
+	int i,len;
+	len = strlen(s)-1;
+	for(i=0;i<strlen(s)/2;i++)
+	{
+		temp=s[i];
+		s[i]=s[len];
+		s[len]=temp;
+		len--;
+	}
+	printf("the reversed string is = %s",s);
+}
+void main()
+{
+	char s[10];
+	int i;
+	printf("Enter String : ");
+	scanf("%s",s);
+	printf("the original string is=%s\n",s);
+	reverse(s);
+}
